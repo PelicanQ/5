d8295d1e08dabc84fbb4defd05e1ad9824833398
ebbawennerklint <ebba.wennerklint@gmail.com>
2023-05-08 08:54:18 +0200

    hej

---
+++ main.py
@@ -0,0 +1,35 @@
+import numpy as np
+import calfem.core as cfc
+import calfem.geometry as cfg
+import calfem.mesh as cfm
+#import calfem.vis_mpl as cfv
+import calfem.utils as cfu
+
+#Materialparametrar, koppar: 1, nylon: 2
+E1 = 128    #GPa
+E2 = 3      #GPa
+ny1 = 0.36
+ny2 = 0.39
+alfa1 = 17.6*10**-6    #1/K
+alfa2 = 80*10**-6      #1/K
+rho1 = 8930     #kg/m^3   
+rho2 = 1100     #kg/m^3
+cp1 = 386       #J/kgK
+cp2 = 1500      #J/kgK
+k1 = 385        #W/mK
+k2 = 0.26       #W/mK
+
+ac = 40         #W/m^2K
+Tinf = 18       #grader C
+h = 10**5       #W/m^2
+
+#Sidor
+L = 5    #mm
+a = 0.1*L
+b = 0.1*L
+c = 0.3*L
+d = 0.05*L
+h = 0.15*L
+t = 0.05*L
+
+#Skapa mesh 
